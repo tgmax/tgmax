2c421d4947a8fada077e9ea195d6c5de7ac81f2c
Temir Gulyayev <58895329+tgmax@users.noreply.github.com>
2020-09-12 22:39:15 -0400

    Add files via upload

---
+++ name_input.py
@@ -0,0 +1,7 @@
+name_guest = input("Please, enter your name here ")
+if name_guest == "Temir":
+    print("Hello Tgmax King! Welcome back!")
+if name_guest == "Shadi":
+    print("Hello Sunshine! You look great!")
+else:
+    print("I don't know you! Please leave now! ")
